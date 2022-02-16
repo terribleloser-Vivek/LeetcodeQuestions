89a9b00ced4657b2e028d0a8211603e42e52d3b1
VIVEK KUMAR SINGH <86074003+terribleloser-Vivek@users.noreply.github.com>
2022-02-16 10:47:45 +0530

    Update 24-swap-nodes-in-pairs.cpp

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -36,5 +36,27 @@ public:
            
         return head;
         
+        /*
+        ListNode* a = NULL;
+        ListNode* b = head;
+        ListNode* c = head->next;
+        
+        while(b!=NULL && c!=NULL){
+            if(a==NULL){
+                b->next = c->next;
+                c->next = b;
+                head = c;
+            }else{
+                a->next = c;
+                b->next = c->next;
+                c->next = b;
+            }
+            a = b;
+            b = b->next;
+            if(b) c = b->next;
+        }
+                return head;
+*/
+        
     }
 };
